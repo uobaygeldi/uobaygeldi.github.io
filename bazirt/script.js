1c130723129c46b74bd8228c0bd29afc832ac24a
menesetere <ozan.baygeldi@metu.edu.tr>
2022-05-14 23:36:07 +0300

    added rewards for waiting

--- bazirt/script.js
+++ bazirt/script.js
@@ -1,3 +1,4 @@
+var timesWaited = 0
 function timer() {
     var totalTime = 600
     document.getElementById("headr").setAttribute("hidden",true)
@@ -5,17 +6,24 @@ function timer() {
     document.getElementById("count").innerHTML = totalTime
     var x = setInterval(function(){
         if(totalTime === 32){
-            document.getElementById("count").innerHTML = "😂" + totalTime-1 + "😂";
-            totalTime = totalTime-1
+            document.getElementById("count").innerHTML = totalTime-1 + "😂";
+            totalTime = totalTime - 1
         }
         else{
             document.getElementById("count").innerHTML = totalTime-1;
-            totalTime = totalTime-1
+            totalTime = totalTime - 1
         };
         if(totalTime === 0){
             clearInterval(x);
             document.getElementById("count").setAttribute("hidden",true);
             document.getElementById("headr").removeAttribute("hidden")
+            timesWaited = timesWaited + 1
+            if (2 < timesWaited & timesWaited < 5) {
+                document.getElementById("zort").innerHTML = "mal"
+            }
+            if (timesWaited > 5) {
+                document.getElementById("zort").innerHTML = "ciddi mal"
+            }
         }
     }, 1000)
 
